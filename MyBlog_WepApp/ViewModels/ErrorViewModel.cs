f53d44676a32fa39cb913bcf47860013f86e2d11
fatihfurkanarslan <arslanfatihfurkan@gmail.com>
2018-05-01 23:12:22 +0300

    2. part

--- MyBlog_WepApp/ViewModels/ErrorViewModel.cs
+++ MyBlog_WepApp/ViewModels/ErrorViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace MyEvernote.WebApp.ViewModels
+namespace MyBlog_WepApp.ViewModels
 {
     public class ErrorViewModel : NotifyViewModelBase<string>
     {
